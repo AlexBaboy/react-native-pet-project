3778eded8d82b954a5aaccb6db728a146880f235
alexandr_b <alexandr_b@polbox.tv>
2023-06-20 16:53:55 +0300

    form edit

--- src/components/AddRecord/index.tsx
+++ src/components/AddRecord/index.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
-import {ScrollView, Text, Button, SafeAreaView, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {ScrollView, Text, Button, SafeAreaView, StyleSheet, TextInput, TouchableOpacity, View} from 'react-native';
+import * as events from "events";
 
 export const AddRecord = () => {
+
+    const [state, setState] = React.useState({
+        title: '',
+        description: '',
+        published: ''
+    })
+
+    const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        const value = evt.target.value;
+        setState({
+            ...state,
+            [evt.target.name]: value
+        });
+    }
 
     const addRecord = () => {
         console.log('13 add record clicked!!!')
@@ -10,6 +25,30 @@ export const AddRecord = () => {
     return (
         <SafeAreaView style={styles.container}>
             <Text>Add record component</Text>
+
+            <TextInput
+                placeholder={'Title'}
+                onChangeText={handleChange}
+                value={state.title}
+            />
+            <TextInput
+                placeholder={'published'}
+                onChangeText={handleChange}
+                value={state.published}
+            />
+            <TextInput
+                placeholder={'Description'}
+                numberOfLines={4}
+                maxLength={80}
+                onChangeText={handleChange}
+                value={state.description}
+            />
+
+            <View>
+                <TouchableOpacity>
+                    <Text>+</Text>
+                </TouchableOpacity>
+            </View>
 
             <ScrollView>
                 <Button title={'Add record'} onPress={addRecord}/>
