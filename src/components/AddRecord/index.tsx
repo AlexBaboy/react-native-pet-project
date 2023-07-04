12681539f93a38c5897dc130c8d97fa71112acad
alexandr_b <alexandr_b@polbox.tv>
2023-07-04 16:03:15 +0300

    form edit

--- src/components/AddRecord/index.tsx
+++ src/components/AddRecord/index.tsx
@@ -13,17 +13,7 @@ import {
 import {Published, RecordState} from "../../store/slices/recordSlice/types";
 import {sharedColors} from "../../shared/styles/colors";
 import {fontSizes} from "../../shared/styles/fonstSizes";
-import { Svg, Circle, Path } from 'react-native-svg';
-
-const PlusIcon = () => (
-    <Svg width={24} height={24} viewBox="0 0 24 24">
-        <Circle cx={12} cy={12} r={11} fill="currentColor" />
-        <Path
-            d="M12 7v5h5v2h-5v5h-2v-5H7v-2h5V7h2z"
-            fill="#FFF" // Цвет внутренней части иконки
-        />
-    </Svg>
-);
+import {PlusIcon} from "../../shared/components/PlusIcon";
 
 interface ExtendedInputProps extends TextInputProps {
     name: keyof RecordState;
@@ -91,10 +81,14 @@ export const AddRecord = () => {
             </View>
 
             <View style={styles.photoBlock}>
-                <Text style={styles.photoBlockTitle}>Photo</Text>
-                <TouchableOpacity>
-                    <PlusIcon />
-                </TouchableOpacity>
+                <View>
+                    <Text style={styles.photoBlockTitle}>Photo</Text>
+                </View>
+                <View style={styles.plusBlockContainer}>
+                    <TouchableOpacity style={styles.plusBlock} >
+                        <PlusIcon />
+                    </TouchableOpacity>
+                </View>
             </View>
 
             <View style={styles.centeredView}>
@@ -163,23 +157,13 @@ const styles = StyleSheet.create({
     title: {
         fontSize: fontSizes['1rem'] * 2,
     },
-    photoBlock: {
-      marginTop: fontSizes['1rem'],
-      borderTopWidth: fontSizes['1rem'],
-      borderTopColor: sharedColors.bgGray
-    },
-    photoBlockTitle: {
-        marginTop: fontSizes['1rem'],
-        color: sharedColors.black
-    },
+
     centeredView: {
         flex: 1,
         justifyContent: 'flex-end',
         alignItems: 'center',
-        backgroundColor: sharedColors.modalOverlayBackground
     },
     modalView: {
-        // margin: 20,
         backgroundColor: sharedColors.white,
         borderTopLeftRadius: fontSizes['1rem'],
         borderTopRightRadius: fontSizes['1rem'],
@@ -221,5 +205,27 @@ const styles = StyleSheet.create({
     },
     optionText: {
 
+    },
+    photoBlock: {
+        marginTop: fontSizes['1rem'],
+        borderTopWidth: fontSizes['1rem'],
+        borderTopColor: sharedColors.bgGray
+    },
+    photoBlockTitle: {
+        marginTop: fontSizes['1rem'],
+        color: sharedColors.black,
+    },
+    plusBlockContainer: {
+        marginVertical: fontSizes['1rem'],
+        padding: 0
+    },
+    plusBlock: {
+        margin: 0,
+        padding: 0,
+        width: 100,
+        height: 100,
+        backgroundColor: sharedColors.bgGray,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
